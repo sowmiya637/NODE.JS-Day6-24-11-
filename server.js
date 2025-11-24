112a2446479ca78cc95d84ea82a5be2d4c02fb83
sowmiya637 <sowmiyaravichandran637@gmail.com>
2025-11-24 19:17:59 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json()); // parse JSON request body
+
+app.post("/users", (req, res) => {
+  console.log(req.body); // Access the sent JSON
+  res.json({ message: "User created", data: req.body });
+});
+
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
