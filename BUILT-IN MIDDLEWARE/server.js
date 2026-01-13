af9e7243c0e6fc65ec245554cd206c998a740350
Rithiha Elangovan <rithihaelangovan@gmail.com>
2026-01-13 21:50:21 +0530

    add the message

--- BUILT-IN MIDDLEWARE/server.js
+++ BUILT-IN MIDDLEWARE/server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 
 app.use(express.json()); // parse JSON request body
-
+// To test this, you can send a POST request to http://localhost:3000/users
+// with a JSON body using a tool like Postman or curl.
 app.post("/users", (req, res) => {
   console.log(req.body); // Access the sent JSON
   res.json({ message: "User created", data: req.body });
 });
 
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+
