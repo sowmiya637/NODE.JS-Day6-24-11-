83c9025cbf1399a80fefa6afd607e8523d1cf7c2
sowmiya637 <sowmiyaravichandran637@gmail.com>
2026-01-13 22:00:29 +0530

    added the comment line

--- BUILT-IN MIDDLEWARE/server.js
+++ BUILT-IN MIDDLEWARE/server.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.json()); // parse JSON request body
 
+// app.post("/users", (req, res) => {
+//   console.log(req.body); // Access the sent JSON
+//   res.json({ message: "User created", data: req.body });
+// });
 app.post("/users", (req, res) => {
   console.log(req.body); // Access the sent JSON
   res.json({ message: "User created", data: req.body });
